02b5688f12b467c721d39f511623f670ab64a23a
YahyaEraltay <eraltay35@gmail.com>
2024-07-08 11:40:11 +0300

    Some fixes

---
+++ common/ApiWithController/GlobalUsings.cs
@@ -0,0 +1,19 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Text;
+global using System.Reflection;
+global using System.IdentityModel.Tokens.Jwt;
+
+global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.OpenApi.Models;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+
+global using Arfware.ArfBlocks.Core;
+global using Arfware.ArfBlocks.Core.Extentions;
+
+global using Application.Configurations;
+global using Infrastructure.RelationalDB;
+global using Infrastructure.Services;
+global using Microsoft.Extensions.DependencyInjection;
--- common/ApiWithController/Program.cs
+++ common/ApiWithController/Program.cs
@@ -1,44 +1,145 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+// For Dotnet-Ef Commands
+var configurations = builder.Configuration.GetSection("ProjectNameConfigurations").Get<ProjectNameConfigurations>();
+var environmentService = new EnvironmentService(configurations.EnvironmentConfiguration);
+var dbContext = new ApplicationDbContext(new CustomDbContextOptions(configurations.RelationalDbConfiguration, environmentService));
+builder.Services.AddSingleton<ApplicationDbContext>(dbContext);
+
+// ArfBlocks Dependencies
+builder.Services.AddArfBlocks(options =>
+{
+    options.ApplicationProjectNamespace = "Application";
+    options.ConfigurationSection = builder.Configuration.GetSection("ProjectNameConfigurations");
+    options.LogLevel = LogLevels.Warning;
+});
+
+string DefaultCorsPolicy = "DefaultCorsPolicy";
+builder.Services.AddCors(options =>
+            {
+                // Development Cors Policy
+                options.AddPolicy(name: DefaultCorsPolicy,
+                    builder =>
+                    {
+                        builder.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                    });
+            });
+
+
+
+// 	// Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.Audience = JwtService.Audience;
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtService.Secret)),
+            ValidateIssuer = false,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+        };
+
+        var handler = new JwtSecurityTokenHandler();
+        handler.InboundClaimTypeMap.Clear();
+    });
+
+// 	// Get Configurations
+environmentService = new EnvironmentService(configurations.EnvironmentConfiguration);
+
+// 	services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddServer(new OpenApiServer() { Url = environmentService.ApiUrl, Description = environmentService.EnvironmentName });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApp.Api", Version = "v1" });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+    // For avoid Scheme Collision for the same name classes
+    c.CustomSchemaIds(x => x.FullName);
+
+    // Define Security Scheme
+    c.AddSecurityDefinition("Bearer", // Name #1
+        new OpenApiSecurityScheme()
+        {
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer"
+        });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+                    {
+                        new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference{
+                                Id = "Bearer", // Name #1
+								Type = ReferenceType.SecurityScheme
+                            }
+                        },new List<string>()
+                    }
+    });
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint($"{environmentService.ApiUrl}/swagger/v1/swagger.json", "TodoApp.Api v1");
+});
 
-app.UseHttpsRedirection();
+app.UseCors(DefaultCorsPolicy);
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseRouting();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+// // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+// public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+// {
+// 	if (env.EnvironmentName == "Development")
+// 	{
+// 		app.UseDeveloperExceptionPage();
+// 	}
+
+// 	app.UseSwagger();
+// 	app.UseSwaggerUI(c =>
+// 	{
+// 		c.SwaggerEndpoint($"{environmentService.ApiUrl}/swagger/v1/swagger.json", "TodoApp.Api v1");
+// 	});
+
+// 	app.UseCors(DefaultCorsPolicy);
+
+// 	app.UseRouting();
+
+// 	app.UseAuthentication();
+// 	app.UseAuthorization();
+
+// 	app.UseEndpoints(endpoints =>
+// 	{
+// 		endpoints.MapControllers()
+// 				 .RequireAuthorization();  // Automatically add [Authorize] to all Controllers
+// 	});
+// }
