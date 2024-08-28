18e894c68d3daf4f21f63f2bda1533eb770e9e86
Yahya ERALTAY <yahya.eraltay@tkteknoloji.com.tr>
2024-08-28 21:47:56 +0300

    Appsettings configurated

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e566decbfe6b9681cc483fb86dc7c22dfd2e03d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a53b4f2c3204fc0770a7b6199f9c1b33d1a5ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
