84e7d63ac549e245bb4fe0a3d00743a5d8302dc9
Yahya Eraltay <yahya.eraltay@tkteknoloji.com.tr>
2025-04-17 15:15:08 +0300

    Created new migration as AddedActionCommentInTodoTaskEntity

--- Domain/Entities/TodoTask.cs
+++ Domain/Entities/TodoTask.cs
@@ -4,6 +4,7 @@ public class TodoTask : BaseEntity
 {
     public string Title { get; set; }
     public string Description { get; set; }
+    public string ActionComment { get; set; }
     public TodoTaskStatus Status { get; set; }
     public DateTime StatusChangedAt { get; set; }
 
---
+++ Infrastructure/Migrations/20250417121030_AddedActionCommentInTodoTaskEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedActionCommentInTodoTaskEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ActionComment",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: new Guid("2bbd5e1e-fe3d-45c7-9e7f-ba0f2e798e1e"),
+                column: "ActionComment",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActionComment",
+                table: "Tasks");
+        }
+    }
+}
