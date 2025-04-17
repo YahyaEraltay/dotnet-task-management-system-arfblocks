53027e4c2149a049372faade32c45ac41eddd7ff
Yahya Eraltay <yahya.eraltay@tkteknoloji.com.tr>
2025-04-17 14:25:07 +0300

    Updated controller namespaces

--- ApiWithController/Controllers/DepartmentsController.cs
+++ ApiWithController/Controllers/DepartmentsController.cs
@@ -1,46 +1,44 @@
-namespace ApiWithController.Controllers
+namespace ApiWithController.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class DepartmentsController : ControllerBase
 {
-	[ApiController]
-	[Route("[controller]")]
-	[Produces("application/json")]
-	public class DepartmentsController : ControllerBase
+	private readonly ArfBlocksRequestOperator _requestOperator;
+
+	public DepartmentsController(ArfBlocksDependencyProvider dependencyProvider)
 	{
-		private readonly ArfBlocksRequestOperator _requestOperator;
+		_requestOperator = new ArfBlocksRequestOperator(dependencyProvider);
+	}
 
-		public DepartmentsController(ArfBlocksDependencyProvider dependencyProvider)
-		{
-			_requestOperator = new ArfBlocksRequestOperator(dependencyProvider);
-		}
+	[HttpPost("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Create.ResponseModel>>> Create(Application.RequestHandlers.Departments.Commands.Create.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Create.Handler>(payload);
+	}
 
-		[HttpPost("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Create.ResponseModel>>> Create(Application.RequestHandlers.Departments.Commands.Create.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Create.Handler>(payload);
-		}
+	[HttpPut("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Update.ResponseModel>>> Update(Application.RequestHandlers.Departments.Commands.Update.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Update.Handler>(payload);
+	}
 
-		[HttpPut("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Update.ResponseModel>>> Update(Application.RequestHandlers.Departments.Commands.Update.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Update.Handler>(payload);
-		}
+	[HttpDelete("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Delete.ResponseModel>>> Delete(Application.RequestHandlers.Departments.Commands.Delete.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Delete.Handler>(payload);
+	}
 
-		[HttpDelete("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Departments.Commands.Delete.ResponseModel>>> Delete(Application.RequestHandlers.Departments.Commands.Delete.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Commands.Delete.Handler>(payload);
-		}
+	[HttpGet("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Departments.Queries.All.ResponseModel>>> All()
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Queries.All.Handler>(null);
+	}
 
-		[HttpGet("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Departments.Queries.All.ResponseModel>>> All()
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Queries.All.Handler>(null);
-		}
-
-		[HttpGet("[action]")]
-		public async Task<ActionResult<Application.RequestHandlers.Departments.Queries.Detail.ResponseModel>> Detail(Application.RequestHandlers.Departments.Queries.Detail.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Queries.Detail.Handler>(payload);
-		}
+	[HttpGet("[action]")]
+	public async Task<ActionResult<Application.RequestHandlers.Departments.Queries.Detail.ResponseModel>> Detail(Application.RequestHandlers.Departments.Queries.Detail.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Departments.Queries.Detail.Handler>(payload);
 	}
 }
-
--- ApiWithController/Controllers/UsersController.cs
+++ ApiWithController/Controllers/UsersController.cs
@@ -1,58 +1,56 @@
-namespace ApiWithController.Controllers
+namespace ApiWithController.Controllers;
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class UsersController : ControllerBase
 {
-	[ApiController]
-	[Route("[controller]")]
-	[Produces("application/json")]
-	public class UsersController : ControllerBase
+	private readonly ArfBlocksRequestOperator _requestOperator;
+
+	public UsersController(ArfBlocksDependencyProvider dependencyProvider)
 	{
-		private readonly ArfBlocksRequestOperator _requestOperator;
+		_requestOperator = new ArfBlocksRequestOperator(dependencyProvider);
+	}
 
-		public UsersController(ArfBlocksDependencyProvider dependencyProvider)
-		{
-			_requestOperator = new ArfBlocksRequestOperator(dependencyProvider);
-		}
+	[HttpPost("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Create.ResponseModel>>> Create(Application.RequestHandlers.Users.Commands.Create.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Create.Handler>(payload);
+	}
 
-		[HttpPost("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Create.ResponseModel>>> Create(Application.RequestHandlers.Users.Commands.Create.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Create.Handler>(payload);
-		}
+	[HttpPut("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Update.ResponseModel>>> Update(Application.RequestHandlers.Users.Commands.Update.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Update.Handler>(payload);
+	}
 
-		[HttpPut("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Update.ResponseModel>>> Update(Application.RequestHandlers.Users.Commands.Update.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Update.Handler>(payload);
-		}
+	[HttpDelete("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Delete.ResponseModel>>> Delete(Application.RequestHandlers.Users.Commands.Delete.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Delete.Handler>(payload);
+	}
 
-		[HttpDelete("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Delete.ResponseModel>>> Delete(Application.RequestHandlers.Users.Commands.Delete.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Delete.Handler>(payload);
-		}
+	[AllowAnonymous]
+	[HttpPost("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Login.ResponseModel>>> Login(Application.RequestHandlers.Users.Commands.Login.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Login.Handler>(payload);
+	}
 
-		[AllowAnonymous]
-		[HttpPost("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Commands.Login.ResponseModel>>> Login(Application.RequestHandlers.Users.Commands.Login.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Commands.Login.Handler>(payload);
-		}
+	[HttpGet("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Queries.All.ResponseModel>>> All()
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.All.Handler>(null);
+	}
 
-		[HttpGet("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Queries.All.ResponseModel>>> All()
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.All.Handler>(null);
-		}
+	[HttpGet("[action]")]
+	public async Task<ActionResult<List<Application.RequestHandlers.Users.Queries.Me.ResponseModel>>> Me()
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.Me.Handler>(null);
+	}
 
-		[HttpGet("[action]")]
-		public async Task<ActionResult<List<Application.RequestHandlers.Users.Queries.Me.ResponseModel>>> Me()
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.Me.Handler>(null);
-		}
-
-		[HttpGet("[action]")]
-		public async Task<ActionResult<Application.RequestHandlers.Users.Queries.Detail.ResponseModel>> Detail(Application.RequestHandlers.Users.Queries.Detail.RequestModel payload)
-		{
-			return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.Detail.Handler>(payload);
-		}
+	[HttpGet("[action]")]
+	public async Task<ActionResult<Application.RequestHandlers.Users.Queries.Detail.ResponseModel>> Detail(Application.RequestHandlers.Users.Queries.Detail.RequestModel payload)
+	{
+		return await _requestOperator.OperateHttpRequest<Application.RequestHandlers.Users.Queries.Detail.Handler>(payload);
 	}
 }
